1bd21242299d78496329343e48512b11fa3a637c
Andy Loughran <andy@lockran.com>
2017-07-06 23:34:05 +0100

    fixed redirect to IdentityPage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -56,7 +56,7 @@ export class LoginPage {
     } else {
       this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
         .then(authData => {
-          this.navCtrl.setRoot('HomePage');
+          this.navCtrl.setRoot('IdentityPage');
         }, error => {
           this.loading.dismiss().then(() => {
             let alert = this.alertCtrl.create({
