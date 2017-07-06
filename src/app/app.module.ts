fadd9946a3d8ef8802df196da976003dc05a6da4
Andy Loughran <andy@lockran.com>
2017-07-06 23:32:14 +0100

    Setup firebase authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -33,6 +34,7 @@ import { firebaseConfig } from './firebaseConfig';
   providers: [
     StatusBar,
     SplashScreen,
+    AuthProvider,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
